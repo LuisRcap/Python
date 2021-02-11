ee55a7bdd4725bb12d6aa1fcaee6986f635a0e13
LuisRcap <luisdany7773@gmail.com>
2021-02-11 15:12:05 -0600

    Try and except Python

---
+++ HelloWorld/try-and-except.py
@@ -0,0 +1,6 @@
+try:
+    if(nombre>3):       #Esto dará un error porque nombre no está definido, entonces saltará al except
+        print("Hola")
+except:
+    print("Algo salió mal con el código, por favor revisalo")
+
