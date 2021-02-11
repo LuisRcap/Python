f566432a06f71b16845b84599b27db3586918f9d
LuisRcap <luisdany7773@gmail.com>
2021-02-10 19:18:47 -0600

    Operadores aritméticos en python

---
+++ HelloWorld/operadores.py
@@ -0,0 +1,22 @@
+numero1 = 15
+numero2 = 25
+
+suma = numero1 + numero2
+
+print("La suma es: ", suma)
+
+resta = numero1 - numero2
+
+print("La resta es: ", resta)
+
+mult = numero1 * numero2
+
+print("La multiplicación es: ", mult)
+
+division = numero1 / numero2
+
+print("La división es: ", division)
+
+modulo = numero2 % numero1
+
+print("El modulo es: ", modulo)
