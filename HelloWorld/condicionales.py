7df387bb3a0e7407ca7cdc7cd3d9fbf3ae78b6bd
LuisRcap <luisdany7773@gmail.com>
2021-02-11 12:18:32 -0600

    Condicionales en Python

---
+++ HelloWorld/condicionales.py
@@ -0,0 +1,33 @@
+if(5 > 3):{
+    print("Hola")
+}
+
+if(3 < 2):{
+    print("Hola")
+}
+else:{
+    print("La condición no es verdadera")
+}
+
+print(3 >= 2)
+print(3 <= 2)
+print(3 == 3)
+print(3 != 2)
+
+age = 16
+
+if(age < 13):{
+    print("Eres joven")
+}
+elif(age >= 13 and age < 18):{
+    print("Eres un adolecente")
+}
+else:{
+    print("Eres un adulto")
+}
+
+if(5 > 3 and 2 > 1):
+    print("Hey")
+
+if(5 > 3 or 2 < 1):
+    print("Hey")
