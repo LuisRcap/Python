d57f1927653f443c325b2c164d91d5779fc7f4cc
LuisRcap <luisdany7773@gmail.com>
2021-02-11 12:47:02 -0600

    Ciclos for en Python

---
+++ HelloWorld/for-loop.py
@@ -0,0 +1,43 @@
+list1 = ["Manzanas", "Platanos", "Cerezas"]
+tup1 = (13,12,15)
+
+for item in list1:{
+    print(item)
+}
+
+print("-------------")
+
+for item in tup1:{
+    print(item)
+}
+
+print("-------------")
+
+for i in range(0,10):{
+    print(i)
+}
+
+print("-------------")
+
+for i in range(1,11):{
+    print(i)
+}
+
+print("-------------")
+
+for i in range(0,11,2):{
+    print(i)
+}
+
+print("-------------")
+
+for i in range(0,51,5):{
+    print(i)
+}
+
+print("-------------")
+
+for i in range(0,5):
+    for j in range(0,3):
+        print(i*j)
+
